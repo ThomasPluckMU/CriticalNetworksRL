83b29676dfee554bf75e433db1c91ac01dd529d2
ThomasPluckMU <thomas.pluck.2025@mumail.ie>
2025-04-23 12:28:56 +0200

    install package in dev mode

--- quick_run.sh
+++ quick_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install python-is-python3 && mkdir logs
-pip install -r requirements.txt && python ./setup.py
+pip install -r requirements.txt && pip install -e .
 nohup python scripts/train_atari.py --config criticalnets/config/ppo_sweep.yaml > ppo_script.log 2>&1 &
 nohup python scripts/train_atari.py --config criticalnets/config/dqn_td_sweep.yaml > dqn_td_script.log 2>&1 &
